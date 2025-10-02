9fef90d66081139a43e176465b457412eef7f8bc
Murodjon <yoshxaker004@gmail.com>
2025-10-02 10:53:40 +0500

    MongoDB ulanish sozlamalari qo‘shildi

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB ga ulanish
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log("✅ MongoDB ulandi"))
   .catch(err => console.error("❌ MongoDB xato:", err));
 
@@ -24,6 +27,5 @@ app.use("/api/users", userRoutes);
 const orderRoutes = require("./routes/orderRoutes");
 app.use("/api/orders", orderRoutes);
 
-// Serverni ishga tushurish
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server ${PORT}-portda ishga tushdi`));
+// 🚀 Vercel uchun app ni export qilamiz
+module.exports = app;
